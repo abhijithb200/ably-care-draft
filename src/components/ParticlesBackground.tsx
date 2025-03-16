0931d0d00a0efdcbaee1fe882fa42c73f0a4c51e
Binnukrishna <binnukrishna5233@gmail.com>
2025-03-16 13:37:53 +0530

    update : particle js added for next js

---
+++ src/components/ParticlesBackground.tsx
@@ -0,0 +1,138 @@
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesBackground: React.FC = () => {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen:{
+        enable: false
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 4,
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4,
+          },
+        },
+      },
+        particles: {
+          number: {
+            value: 80,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          color: {
+            value: "#ffffff"
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 0.5,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 1,
+              opacity_min: 0.1,
+              sync: false
+            }
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 40,
+              size_min: 0.1,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 6,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 1200
+            }
+          }
+        },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        options={options}
+        width={"100px"}
+        height={"100px"}
+      />
+    );
+  }
+
+  return <></>;
+};
+
+export default ParticlesBackground;
--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,10 +1,43 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import ParticlesBackground from "../ParticlesBackground";
+
 
 const Hero = () => {
   return (
     <>
-        Hero
+      <main className="w-full h-screen md:h-[500px] md:max-h-[800px] flex justify-center items-center">
+        <div className="relative w-full h-full lg:max-w-7xl rounded-3xl">
+          <div className="w-full h-full z-[3] flex flex-col items-start justify-center p-4 gap-3">
+            <h1 className="font-poppins text-white text-4xl font-bold">Compassionate Care, Tailored for You</h1>
+            <p className="text-white text-xs font-extralight font-inter">
+              At Ably Care, we provide personalized, high-quality healthcare
+              services with warmth and dedication. Whether it's disability
+              support, aged care, or mental well-being, our expert team is here
+              to empower and uplift every individual. Your care, your way
+              because you deserve the best
+            </p>
+            <Button className="bg-customAccent text-white hover:bg-customAccent/80 rounded-xl">See More</Button>
+          </div>
+          <div className="absolute top-0 left-0 w-full h-full lg:max-w-7xl rounded-3xl overflow-hidden z-[-1]">
+            <div className="absolute w-full h-full bg-gradient-to-t from-black/70 to-transparent z-[3]"></div>
+            <div className="absolute w-full h-full z-[2] overflow-hidden">
+              <ParticlesBackground/>
+            </div>
+            <div className="absolute w-full h-full z-[1]">
+              <Image
+                src="/images/hero-bg.jpg"
+                alt="Ably Care Logo"
+                fill
+                className="absolute w-full h-full object-cover"
+              />
+            </div>
+          </div>
+          image
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
