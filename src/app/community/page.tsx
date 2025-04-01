7245d2711daa618ee768566edc4de0484c28978e
Binnukrishna <binnukrishna5233@gmail.com>
2025-04-01 17:12:55 +0530

    update : community hero udpated

---
+++ src/app/community/page.tsx
@@ -0,0 +1,14 @@
+import CommunityHero from "@/components/community/CommunityHero"
+
+
+const Community = () => {
+  return (
+    <>
+        <main className="mx-auto p-2 w-full lg:max-w-7xl mt-20">
+            <CommunityHero />
+        </main>
+    </>
+  )
+}
+
+export default Community
